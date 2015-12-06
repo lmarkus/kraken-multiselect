5fbd06f83e1ba4bef8095217a64908eb5d4a5bcd
lmarkus <lmarkus@paypal.com>
2015-12-05 23:33:36 -0800

    Added handler for excuses

--- controllers/index.js
+++ controllers/index.js
@@ -8,11 +8,31 @@ module.exports = function (router) {
     var model = new IndexModel();
 
     router.get('/', function (req, res) {
-        
-        
+
+
         res.render('index', model);
-        
-        
+
+
+    });
+
+    router.post('/', function (req, res) {
+
+        var excuse = req.body.excuse;
+
+        if (!excuse) {
+            //Nothing was selected
+        }
+        else if (Array.isArray(excuse)) {
+            //Multiple excuses were selected
+        }
+        else {
+            //A single excuse was selected
+        }
+
+        //Inject these into the model for rendering.
+        model.receivedExcuse = excuse;
+
+        res.render('index',model)
     });
 
 };
